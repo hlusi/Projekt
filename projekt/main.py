44a8076f1495a4f061d9e7e5fbf28ece5ac5f7b4
Veronika <xhlusi01@vutbr.cz>
2020-04-06 19:48:01 +0200

    zacátek projektu

--- projekt/main.py
+++ projekt/main.py
@@ -1,3 +1,35 @@
-list=[1,5,6,7,8]
-a=max(list)
-print(a)
+import os
+data = open('data.txt','r')
+text = data.read()
+file_status = os.stat('data.txt')
+print(file_status)
+# kontroluju velikost souboru
+if file_status.st_size < 1:
+    print('Soubor muze byt prazdny')
+else: print('zadana data maji spravnou delku')
+
+# konrojulu datovy typ
+x = 'dgtjy'
+if type(text) == type(x):
+   text = text
+else:
+    text = str(text)
+
+# upravovani delky textu
+padded = text + ((16 - len(text) % 16) * chr(16 - len(text) % 16))
+print(padded)
+
+#prevod dat do vhodne datove struktury
+text_v_bytech = str.encode(padded)
+
+print(text_v_bytech)
+print(len(text_v_bytech))
+
+
+
+
+
+
+
+
+
--- untitled/main.py
+++
@@ -1,3 +0,0 @@
-list=[1,5,6,7,8]
-a=max(list)
-print(a)
